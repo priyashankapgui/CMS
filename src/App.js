b93b1fa20f045f7f05b302a782a4633c5978a7c1
shalu2001 <danthanarayanas.21@uom.lk>
2024-02-17 19:31:31 +0530

    Revert "Merge branch 'login' into dev"
    This reverts commit f887ba28f084b44b5860471b270e7a5ac830fabb, reversing
    changes made to d99aae546f04a364430f29146b6c56e1680cf232.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+
 import './App.css';
-import createRoutes from './routes';
 
 function App() {
   return (
-    <div>
-      <Router>
-        {createRoutes()}
-      </Router>
+    <div className="App">
+      
     </div>
   );
 }
