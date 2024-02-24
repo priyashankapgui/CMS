0e7ff7864a5660fc496ffa7279af0b239dc46ee9
Achala Imesh <imeshbaa.21@uom.lk>
2024-02-24 12:54:46 +0530

    Update 2

--- src/pages/Billing/Sales/Sales.jsx
+++ src/pages/Billing/Sales/Sales.jsx
@@ -3,15 +3,16 @@ import "./Sales.css";
 
 export const Sales = () => {
     return (
-        <>
+        <div>
             <div className="sales">
                 <h4>Sales</h4>
             </div>
             <Layout>
                 <div>
-                    <p>This is the body content of the Sales page.</p>
+                    <p>This is the body content of the Sales page ndmhafhkfhkfahkhfkhk.</p>
                 </div>
             </Layout>
-        </>
+
+        </div>
     );
 };
