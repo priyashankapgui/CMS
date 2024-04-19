4c86340d9b71cb54b27e0c6c721399b94c624103
Achala Imesh <imeshbaa.21@uom.lk>
2024-04-19 22:43:31 +0530

    Update 5.9

--- src/pages/Billing/WorkList/WorkList-Billed/ViewBill.jsx
+++ src/pages/Billing/WorkList/WorkList-Billed/ViewBill.jsx
@@ -1,6 +1,7 @@
-import { React } from 'react'
-import "./ViewBill.css";
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
 import Layout from "../../../../Layout/Layout";
+import "./ViewBill.css";
 import InputLabel from '../../../../Components/Label/InputLabel';
 import { RiPrinterFill } from "react-icons/ri";
 import { MdOutlineAssignmentReturn } from "react-icons/md";
@@ -9,15 +10,25 @@ import InputField from '../../../../Components/InputField/InputField';
 import InputDropdown from '../../../../Components/InputDropdown/InputDropdown';
 import RoundButtons from '../../../../Components/Buttons/RoundButtons/RoundButtons';
 import { IoChevronBackCircleOutline } from "react-icons/io5";
-import { Link } from 'react-router-dom';
+import jsonData from '../../../../Components/Data.json';
 
 export const ViewBill = () => {
+    const { billNo } = useParams();
+    const selectedBillData = jsonData.worklistTableData.find(bill => bill.billNo === billNo);
+
+    if (!selectedBillData) {
+        return <div>Bill not found</div>;
+    }
+
+    const { branch, billedAt, billedBy, customerName, status, paymentMethod, contactNo } = selectedBillData;
 
     return (
         <>
             <div className="viewbill">
                 <div className="viewbill-top-link">
-                    <Link to="/work-list"> <IoChevronBackCircleOutline style={{ fontSize: "22px", color: "#0377A8" }} /> </Link>
+                    <Link to="/work-list">
+                        <IoChevronBackCircleOutline style={{ fontSize: "22px", color: "#0377A8" }} />
+                    </Link>
                     <h4>View Bill</h4>
                 </div>
             </div>
@@ -26,39 +37,33 @@ export const ViewBill = () => {
                     <div className='view-bill-top-cont'>
                         <div className="cont1">
                             <div className='inputFlex'>
-                                <InputLabel for="branchName" color="#0377A8">Branch: </InputLabel>
-
+                                <InputLabel for="branchName" color="#0377A8">Branch: <span>{branch}</span></InputLabel>
                             </div>
                             <div className='inputFlex'>
-                                <InputLabel for="billNo" color="#0377A8">Bill No: </InputLabel>
+                                <InputLabel for="billNo" color="#0377A8">Bill No: <span>{billNo}</span></InputLabel>
                             </div>
                             <div className='inputFlex'>
-                                <InputLabel for="status" color="#0377A8">Status: </InputLabel>
+                                <InputLabel for="status" color="#0377A8">Status: <span>{status}</span></InputLabel>
                             </div>
                         </div>
-
                         <div className="cont2">
                             <div className='inputFlex'>
-                                <InputLabel for="billedAt" color="#0377A8">Billed At: </InputLabel>
-
+                                <InputLabel for="billedAt" color="#0377A8">Billed At: <span>{billedAt}</span></InputLabel>
                             </div>
                             <div className='inputFlex'>
-                                <InputLabel for="billedBy" color="#0377A8">Billed By: </InputLabel>
+                                <InputLabel for="billedBy" color="#0377A8">Billed By: <span>{billedBy}</span></InputLabel>
                             </div>
                             <div className='inputFlex'>
-                                <InputLabel for="paymentMethod" color="#0377A8">Payment Method: </InputLabel>
+                                <InputLabel for="paymentMethod" color="#0377A8">Payment Method: <span>{paymentMethod}</span></InputLabel>
                             </div>
                         </div>
-
                         <div className="cont3">
                             <div className='inputFlex'>
-                                <InputLabel for="cusName" color="#0377A8">Customer Name: </InputLabel>
-
+                                <InputLabel for="cusName" color="#0377A8">Customer Name: <span>{customerName}</span></InputLabel>
                             </div>
                             <div className='inputFlex'>
-                                <InputLabel for="cusContact" color="#0377A8">Contact No: </InputLabel>
+                                <InputLabel for="cusContact" color="#0377A8">Contact No: <span>{contactNo}</span></InputLabel>
                             </div>
-
                         </div>
                     </div>
                     <hr />
@@ -77,11 +82,8 @@ export const ViewBill = () => {
                             <InputLabel> Cancel Bill </InputLabel>
                             <RoundButtons id="cancelBillBtn" type="submit" name="cancelBillBtn" backgroundColor="#EB1313" icon={<AiOutlineClose style={{ color: 'white' }} onClick={() => console.log("Cancel Bill Button clicked")} />} />
                         </div>
-
                     </div>
                 </div>
-
-
                 <div className="billed-item-container">
                     <table className='billed-item-table'>
                         <thead>
@@ -95,14 +97,16 @@ export const ViewBill = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td><InputField type="text" id="" name="productName" editable={false} width="300px" value={""} /></td>
-                                <td><InputField type="text" id="" name="billQty" editable={false} width="100%" value={""} /></td>
-                                <td><InputDropdown id="" name="batchNo" width="100%" options={['',]} editable={false} value={""} /></td>
-                                <td><InputField type="text" id="" name="unitPrice" editable={false} width="100%" value={""} /></td>
-                                <td><InputField type="text" id="" name="discount" editable={false} width="100%" value={""} /></td>
-                                <td><InputField type="text" id="" name="amount" editable={false} width="100%" value={""} /></td>
-                            </tr>
+                            {selectedBillData.billedItems.map((item, index) => (
+                                <tr key={index}>
+                                    <td><InputField id="" name="productName" editable={false} width="300px" value={item.name} /></td>
+                                    <td><InputField id="" name="billQty" editable={false} width="100%" value={item.quantity} textAlign='center' /></td>
+                                    <td><InputDropdown id="" name="batchNo" width="100%" options={[item.batchNo]} editable={true} /></td>
+                                    <td><InputField id="" name="unitPrice" editable={false} width="100%" value={item.rate} textAlign='right' /></td>
+                                    <td><InputField id="" name="discount" editable={false} width="100%" value={""} textAlign='right' /></td>
+                                    <td><InputField id="" name="amount" editable={false} width="100%" value={(item.rate * item.quantity).toFixed(2)} textAlign='right' /></td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
@@ -113,38 +117,43 @@ export const ViewBill = () => {
                             <tbody>
                                 <tr>
                                     <td><InputLabel for="grossTotal" color="#0377A8">Gross Total</InputLabel></td>
-                                    <td><InputField type="text" id="grossTotal" name="grossTotal" editable={false} marginTop="0" value={""} /></td>
+                                    <td><InputField type="text" id="grossTotal" name="grossTotal" editable={false} marginTop="0" textAlign='right' value={""} /></td>
                                 </tr>
                                 <tr>
                                     <td><InputLabel for="discount" color="#0377A8">Discount %</InputLabel></td>
                                     <td>
                                         <div className="discount-fields-container-viewbill">
-                                            <InputField type="text" id="discountRate" name="discountRate" className="discountRate" editable={true} placeholder="%" width="3em" value={""} />
-                                            <InputField type="text" id="discountAmount" name="discountAmount" className="discountAmount" editable={false} width="23.7em" />
+                                            <InputField type="text" id="discountRate" name="discountRate" className="discountRate" editable={true} placeholder="%" width="3em" textAlign='right' value={""} />
+                                            <InputField type="text" id="discountAmount" name="discountAmount" className="discountAmount" editable={false} width="23.7em" textAlign='right' value={""}/>
                                         </div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td><InputLabel for="netTotal" color="#0377A8" fontsize="1.125em" fontweight="510">Net Total</InputLabel></td>
-                                    <td><InputField type="text" id="netTotal" name="netTotal" editable={false} marginTop="0" value={""} /></td>
+                                    <td><InputField type="text" id="netTotal" name="netTotal" editable={false} marginTop="0" textAlign='right' value={""} /></td>
                                 </tr>
                                 <tr>
                                     <td><InputLabel for="received" color="#0377A8">Received</InputLabel></td>
-                                    <td><InputField type="text" id="received" name="received" editable={false} marginTop="0" value={""} /></td>
+                                    <td><InputField type="text" id="received" name="received" editable={false} marginTop="0" textAlign='right' value={""} /></td>
                                 </tr>
                                 <tr>
                                     <td><InputLabel for="balance" color="#0377A8">Balance</InputLabel></td>
-                                    <td><InputField type="text" id="balance" name="balance" editable={false} marginTop="0" value={""} /></td>
+                                    <td><InputField type="text" id="balance" name="balance" editable={false} marginTop="0" textAlign='right' value={""} /></td>
                                 </tr>
                                 <tr>
-                                    <td colSpan="2"><InputLabel for="noQty" color="#0377A8">No Qty:</InputLabel></td>
+                                    <td colSpan="2"><InputLabel for="noQty" color="#0377A8">No Qty:<span> </span></InputLabel></td>
                                 </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
-            </Layout>
 
+
+
+
+            </Layout>
         </>
     );
 };
+
+export default ViewBill;
