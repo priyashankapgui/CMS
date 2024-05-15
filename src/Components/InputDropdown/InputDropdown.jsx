96f93019087f8566e2b00c1ec725b3bfe8f1a1a1
shalu2001 <danthanarayanas.21@uom.lk>
2024-05-15 13:26:19 +0530

    Merge from login-sdev2 - Filtering in Accounts Page

--- src/Components/InputDropdown/InputDropdown.jsx
+++ src/Components/InputDropdown/InputDropdown.jsx
@@ -1,33 +1,52 @@
-import React from 'react';
-import './InputDropdown.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-const InputDropdown = ({ id, name, height, width, onChange, editable, options, marginTop, borderRadius }) => {
-    if (!options || !Array.isArray(options) || options.length === 0) {
-        return <div>Data not available</div>;
-    }
+const Dropdown = styled.select`
+  border-radius: 0.625em;
+  border: 1px solid #8D9093;
+  height: ${({ height }) => height || '2.25em'};
+  width: ${({ width }) => width || '15.625em'};
+  margin-top: 0.313em;
+  margin-bottom: 0.313em;
+  font-size: 0.813em;
+  padding-left: 0.625em;
+  opacity: ${({ editable }) => (editable ? 1 : 0.5)};
+  pointer-events: ${({ editable }) => (editable ? 'auto' : 'none')};
+`;
 
-    return (
-        <div>
-            <select
-                id={id}
-                name={name}
-                style={{
-                    borderRadius: borderRadius || '0.625em',
-                    height: height || '2.25em',
-                    width: width || '15.625em',
-                    marginTop: marginTop || '0.313em',
-                }}
-                className={`dropdown ${editable ? '' : 'disabled'}`}
-                onChange={onChange}
-            >
-                {options.map((option, index) => (
-                    <option key={index} value={option}>
-                        {option}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
+const InputDropdown = ({ id, name, style, height, width, onChange, editable, options }) => {
+        const [selectedOption, setSelectedOption] = useState('');
+
+        const handleOptionChange = (event) => {
+                const selectedValue = event.target.value;
+                setSelectedOption(selectedValue);
+                onChange(selectedValue); // Pass the selected value to the parent component
+        };
+
+        if (!options || !Array.isArray(options) || options.length === 0) {
+                return <div>No options available</div>;
+        }
+
+        return (
+                <div>
+                        <Dropdown
+                                id={id}
+                                name={name}
+                                style={style}
+                                height={height}
+                                width={width}
+                                onChange={handleOptionChange} // Call the handleOptionChange function on change
+                                editable={editable}
+                                value={selectedOption} // Set the selected value of the dropdown
+                        >
+                                {options.map((option, index) => (
+                                        <option key={index} value={option}>
+                                                {option}
+                                        </option>
+                                ))}
+                        </Dropdown>
+                </div>
+        );
 };
 
 export default InputDropdown;
