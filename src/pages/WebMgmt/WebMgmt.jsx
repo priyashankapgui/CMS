eb5af9467786431e0dba9d18f3c05cb866de25ff
PRIYASHANKA.P.G.U.I <110629606+priyashankapgui@users.noreply.github.com>
2024-06-17 19:22:26 +0530

    new

--- src/pages/WebMgmt/WebMgmt.jsx
+++ src/pages/WebMgmt/WebMgmt.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from "../../Layout/Layout";
 import './WebMgmt.css';
+import InputLabel from '../../Components/Label/InputLabel';
 
 export const WebMgmt = () => {
-  
+    const [images, setImages] = useState([]);
+
+    const handleImageChange = (e) => {
+        const files = Array.from(e.target.files);
+        setImages(files);
+    };
 
     return (
         <>
-            
-           
             <Layout>
-
-            <div className="web-mgmt-container">
-            <div>
-                <h3>Home Page Images</h3>
-                
-            </div>
-         
-
-
-
-             </div>
-
+                <div className="web-mgmt-container">
+                    <div>
+                        <h3 className='web-mgmt-headerOne'>Home Page Images</h3>
+                        <div className="web-mgmt-data">
+                            <InputLabel htmlFor="uploadImages" color="#0377A8" >Upload Carousel Images</InputLabel>
+                            <input 
+                                type="file" 
+                                id="uploadCarosalImages" 
+                                name="images" 
+                                style={{ width: '100%' }} 
+                                className="web-mgmt-lable"
+                                onChange={handleImageChange} 
+                                multiple 
+                            />
+                        </div>
+                        <div className="web-mgmt-data">
+                        <InputLabel htmlFor="uploadImages" color="#0377A8">Upload End Images</InputLabel>
+                        <input 
+                                type="file" 
+                                id="uploadEndImages" 
+                                name="images" 
+                                className="web-mgmt-lable"
+                                style={{ width: '100%' }} 
+                                onChange={handleImageChange} 
+                                multiple 
+                            />
+                        </div>
+                        <div>
+                        <InputLabel htmlFor="uploadImages" color="#0377A8">Edit the Footer</InputLabel>
+                        
+                        </div>
+                    </div>
+                </div>
             </Layout>
         </>
     );
