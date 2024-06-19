16ddbf4ef049678d0875122c201e33f62a4032f6
PRIYASHANKA.P.G.U.I <110629606+priyashankapgui@users.noreply.github.com>
2024-06-19 11:28:58 +0530

    new19/6

--- src/pages/WebMgmt/WebMgmt.jsx
+++ src/pages/WebMgmt/WebMgmt.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Layout from "../../Layout/Layout";
 import './WebMgmt.css';
 import InputLabel from '../../Components/Label/InputLabel';
+import InputField from '../../Components/InputField/InputField';
 
 export const WebMgmt = () => {
     const [images, setImages] = useState([]);
@@ -16,7 +17,7 @@ export const WebMgmt = () => {
             <Layout>
                 <div className="web-mgmt-container">
                     <div>
-                        <h3 className='web-mgmt-headerOne'>Home Page Images</h3>
+                        <h3 className='web-mgmt-headerOne'>Home Page </h3>
                         <div className="web-mgmt-data">
                             <InputLabel htmlFor="uploadImages" color="#0377A8" >Upload Carousel Images</InputLabel>
                             <input 
@@ -41,10 +42,29 @@ export const WebMgmt = () => {
                                 multiple 
                             />
                         </div>
-                        <div>
-                        <InputLabel htmlFor="uploadImages" color="#0377A8">Edit the Footer</InputLabel>
+                        <div  className="web-mgmt-data">
+                        <InputLabel htmlFor="AddAnimetion" color="#0377A8">Add animetion</InputLabel>
+
                         
                         </div>
+
+                        <h3 className='web-mgmt-headerOne'>AboutUs Page </h3>
+                        <div className="web-mgmt-data">
+                            <InputLabel htmlFor="uploadImages" color="#0377A8" >Upload AboutUs Image</InputLabel>
+                            <input 
+                                type="file" 
+                                id="uploadCarosalImages" 
+                                name="images" 
+                                style={{ width: '100%' }} 
+                                className="web-mgmt-lable"
+                                onChange={handleImageChange} 
+                                multiple 
+                            />
+                         </div>
+                         <div className="web-mgmt-data">
+                            <InputLabel htmlFor="uploadImages" color="#0377A8" >Upload AboutUs</InputLabel>
+                            <InputField type="text" id="aboutUs" name="AboutUs" textAlign="left" editable={true} width="100%"  height="20vh"/>
+                         </div>
                     </div>
                 </div>
             </Layout>
