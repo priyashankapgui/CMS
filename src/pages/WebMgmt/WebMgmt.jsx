d30e9aab72bc765d24a02b0bdca98b292e281305
PRIYASHANKA.P.G.U.I <110629606+priyashankapgui@users.noreply.github.com>
2024-06-24 16:47:52 +0530

    new

--- src/pages/WebMgmt/WebMgmt.jsx
+++ src/pages/WebMgmt/WebMgmt.jsx
@@ -1,148 +1,168 @@
 import React, { useState } from "react";
 import Layout from "../../Layout/Layout";
-import { useDropzone } from "react-dropzone";
 import "./WebMgmt.css";
 import InputLabel from "../../Components/Label/InputLabel";
-import InputField from "../../Components/InputField/InputField";
 import Buttons from "../../Components/Buttons/SquareButtons/Buttons";
+import InputFile from "../../Components/InputFile/InputFile";
+import CustomAlert from "../../Components/Alerts/CustomAlert/CustomAlert";
+import { MdSettingsSuggest } from "react-icons/md";
 
 export const WebMgmt = () => {
-    const [imageUrl, setImageUrl] = useState(null);
-    const [files, setFile] = useState(null);
-    const [imageType, setImageType] = useState(""); 
-    const formData = new FormData();
-    const [showAlertSuccess, setShowAlertSuccess] = useState(false);
-    const [showAlertError, setShowAlertError] = useState(false);
-    const [loading, setLoading] = useState(false);
-  
-    const handleImageChange = (event, type) => {
-      const file = event.target.files;
-      setFile(file);
-      setImageType(type); 
-    };
-  
-    const handleCreatewebImage = async () => {
-      setLoading(true);
-      try {
-        if (files) {
-          for (var x = 0; x < files.length; x++) {
-            formData.append("images", files[x]);
-          }
-          formData.append("type", imageType); 
+  const [imageUrl, setImageUrl] = useState(null);
+  const [files, setFiles] = useState({ carosel: null, endimage: null, aboutimages: null });
+  const [imageType, setImageType] = useState("");
+  const formData = new FormData();
+  const [alert, setAlert] = useState({ severity: "", title: "", message: "", show: false });
+  const [loading, setLoading] = useState(false);
+  const [submitted, setSubmitted] = useState({ carosel: false, endimage: false, aboutimages: false });
+
+  const handleImageChange = (event, type) => {
+    const file = event.target.files;
+    setFiles(prevFiles => ({ ...prevFiles, [type]: file }));
+    setImageType(type);
+  };
+
+  const handleCreatewebImage = async () => {
+    setLoading(true);
+    try {
+      if (files[imageType]) {
+        for (var x = 0; x < files[imageType].length; x++) {
+          formData.append("images", files[imageType][x]);
         }
-  
-        const response = await fetch("http://localhost:8080/webIamges", {
-          method: "POST",
-          body: formData,
-        });
-  
-        const contentType = response.headers.get("content-type");
-        if (contentType && contentType.includes("application/json")) {
-          const data = await response.json();
-          if (!response.ok) {
-            console.log("Error:", data.error);
-          } else {
-            setShowAlertSuccess(true);
-            console.log("Success");
-          }
-        } else {
-          const text = await response.text();
-          console.log("Non-JSON Response:", text);
-          setShowAlertError("Unexpected response from server");
-        }
-      } catch (error) {
-        setShowAlertError(error.message);
-        console.error("Error:", error);
+        formData.append("type", imageType);
       }
-      setLoading(false);
-    };
-  
-    return (
-      <Layout>
-        <div className="web-mgmt-container">
-          <div>
-            <h3 className="web-mgmt-header">Home Page</h3>
-            <div className="web-mgmt-data">
-              <InputLabel htmlFor="uploadCarosalImages" color="#0377A8">
-                Upload Carousel Images
-              </InputLabel>
-              <input
-                type="file"
-                id="uploadCarosalImages"
-                name="carosel"
-                style={{ width: "100%" }}
-                className="web-mgmt-lable"
-                onChange={(event) => handleImageChange(event, "carosel")}
-                multiple
-              />
-              <Buttons
-                type="submit"
-                id="submit-btn"
-                style={{ backgroundColor: "white", color: "#EB1313" }}
-                onClick={handleCreatewebImage}
-              >
-                Submit
-              </Buttons>
-            </div>
-  
-            <div className="web-mgmt-data">
-              <InputLabel htmlFor="uploadEndImages" color="#0377A8">
-                Upload End Images
-              </InputLabel>
-              <input
-                type="file"
-                id="uploadEndImages"
-                name="images"
-                className="web-mgmt-lable"
-                style={{ width: "100%" }}
-                onChange={(event) => handleImageChange(event, "endimage")}
-                multiple
-                src={imageUrl}
-              />
-              <Buttons
-                type="submit"
-                id="submit-btn"
-                style={{ backgroundColor: "white", color: "#EB1313" }}
-                onClick={handleCreatewebImage}
-              >
-                Submit
-              </Buttons>
-            </div>
-          </div>
-  
-          <div>
-            <h3 className="web-mgmt-header">AboutUs Page</h3>
-            <div className="web-mgmt-data">
-              <InputLabel htmlFor="uploadAboutUsImages" color="#0377A8">
-                Upload AboutUs Image
-              </InputLabel>
-              <input
-                type="file"
-                id="uploadAboutUsImages"
-                name="images"
-                style={{ width: "100%" }}
-                className="web-mgmt-lable"
-                onChange={(event) => handleImageChange(event, "aboutimages")}
-                multiple
-              />
-              <Buttons
-                type="submit"
-                id="submit-btn"
-                style={{ backgroundColor: "white", color: "#EB1313" }}
-                onClick={handleCreatewebImage}
-              >
-                Submit
-              </Buttons>
-            </div>
-          </div>
-  
-          {loading && <p>Loading...</p>}
-          {showAlertSuccess && <p>Image uploaded successfully!</p>}
-          {showAlertError && (
-            <p className="webMgmt_uploa">Error uploading image</p>
+
+      const response = await fetch("http://localhost:8080/webIamges", {
+        method: "POST",
+        body: formData,
+      });
+
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.includes("application/json")) {
+        const data = await response.json();
+        if (!response.ok) {
+          setAlert({ severity: "error", title: "Error", message: data.error, show: true });
+        } else {
+          setAlert({ severity: "success", title: "Success", message: "Image uploaded successfully!", show: true });
+          setSubmitted(prevSubmitted => ({ ...prevSubmitted, [imageType]: true }));
+        }
+      } else {
+        const text = await response.text();
+        setAlert({ severity: "error", title: "Error", message: "Unexpected response from server", show: true });
+      }
+    } catch (error) {
+      setAlert({ severity: "error", title: "Error", message: error.message, show: true });
+    }
+    setLoading(false);
+  };
+
+  const handleCloseAlert = () => {
+    setAlert(prevAlert => ({ ...prevAlert, show: false }));
+  };
+
+  return (
+    <Layout>
+      <div className="web-mgmt-container">
+        <div className="web-mgmt-Section1">
+          <h4 className="web-mgmt-heading">
+            Home Page <MdSettingsSuggest />
+          </h4>
+          <InputLabel htmlFor="uploadCarosalImages" color="#0377A8">
+            Upload Carousel Images
+          </InputLabel>
+          <InputFile
+            id="uploadCarosalImages"
+            name="carosel"
+            className="web-mgmt-lable"
+            onChange={(event) => handleImageChange(event, "carosel")}
+            multiple
+          />
+          {imageUrl && <img src={imageUrl} alt="Uploaded" />}
+
+          {files.carosel && !submitted.carosel && (
+            <Buttons
+              type="submit"
+              id="submit-btn"
+              style={{ backgroundColor: "#23A3DA", color: "white" }}
+              btnWidth="100%"
+              onClick={handleCreatewebImage}
+            >
+              {loading ? "Uploading..." : "Submit"}
+            </Buttons>
           )}
         </div>
-     </Layout>
-    );
-  };
-  
+
+        <div className="web-mgmt-Section2">
+          <InputLabel htmlFor="uploadEndImages" color="#0377A8">
+            Upload End Images
+          </InputLabel>
+          <InputFile
+            id="uploadEndImages"
+            name="images"
+            className="web-mgmt-lable"
+            onChange={(event) => handleImageChange(event, "endimage")}
+            multiple
+          />
+          {imageUrl && <img src={imageUrl} alt="Uploaded" />}
+
+          {files.endimage && !submitted.endimage && (
+            <Buttons
+              type="submit"
+              id="submit-btn"
+              btnWidth="100%"
+              style={{ backgroundColor: "#23A3DA", color: "white" }}
+              onClick={handleCreatewebImage}
+            >
+              {loading ? "Uploading..." : "Submit"}
+            </Buttons>
+          )}
+        </div>
+
+        <hr className="web-mgmt-hr-line" />
+
+        <div className="web-mgmt-Section3">
+          <h4 className="web-mgmt-heading">
+            About Us Page <MdSettingsSuggest />
+          </h4>
+          <InputLabel htmlFor="uploadAboutUsImages" color="#0377A8">
+            Upload About Us Image
+          </InputLabel>
+
+          <InputFile
+            id="uploadAboutUsImages"
+            name="images"
+            className="web-mgmt-lable"
+            onChange={(event) => handleImageChange(event, "aboutimages")}
+            multiple
+          />
+          {imageUrl && <img src={imageUrl} alt="Uploaded" />}
+
+          {files.aboutimages && !submitted.aboutimages && (
+            <Buttons
+              type="submit"
+              id="submit-btn"
+              btnWidth="100%"
+              style={{ backgroundColor: "#23A3DA", color: "white" }}
+              onClick={handleCreatewebImage}
+            >
+              {loading ? "Uploading..." : "Submit"}
+            </Buttons>
+          )}
+        </div>
+
+        {loading && <p>Uploading...</p>}
+        {alert.show && (
+          <CustomAlert
+            severity={alert.severity}
+            title={alert.title}
+            message={alert.message}
+            duration={4000}
+            onClose={handleCloseAlert}
+          />
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+
